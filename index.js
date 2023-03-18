dc687042499d6f4d23c43c2c8c80275a56cf68ea
Tina Shi <init.defaultBranch>
2023-03-18 16:48:09 +1100

    commit tree 73a42a80cc206a9824c8eac2f3c776bccc69b881

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'karen';
+
+function upperCaseCustomerName() {
+   return customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'kyle';
+}
